cfcb3ac468158b8482bbf5607272f5e14f2e2a78
Vee <vharbison1@gmail.com>
2019-09-21 10:34:30 -0400

    Object add

---
+++ DC/9_21_2019_class_notes/person.js
@@ -0,0 +1,17 @@
+var person = {
+    firstname : "John", 
+    last_name: "Doe",
+    middle_name:"Henry",
+    age:48,
+    likes : ["Sushi", "Jet Skiing", "Beer"]
+};
+
+console.log(person.first_name);
+
+console.log(person["last_name"]);
+
+console.log(person.middle_name);
+
+console.log(person.age);
+
+console.log(person.likes);
